dbc718462e354cc9de7478cb845639a38d2f514c
exnihlio <dace-tree-0r@icloud.com>
2023-05-04 09:53:43 -0600

    fix: correct missing variables

--- terraform/dev.tfvars
+++ terraform/dev.tfvars
@@ -1,5 +1,6 @@
 environment = "dev"
 region = "us-west-2"
+short_environment = "d"
 short_region = "usw2"
 bucket_name = "example-197t371t"
 bucket_force_destroy = true
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,21 @@ variable "bucket_name" {
 variable "bucket_force_destroy" {
     type = bool
 }
+variable "bucket_control_object_ownership" {
+    type = bool
+}
+variable "bucket_object_ownership" {
+    type = bool
+}
+variable "environment" {
+    type = string
+}
+variable "region" {
+    type = string
+}
+variable "short_region" {
+    type = string
+}
+variable "short_environment" {
+    type = string
+}
