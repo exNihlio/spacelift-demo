807d6833d5aa2d20433aedc7ebe33e61cceff12d
exnihlio <dace-tree-0r@icloud.com>
2023-05-04 09:19:41 -0600

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+
+}
+
+module "s3_bucket" {
+    source = "terraform-aws-modules/s3-bucket/aws"
+    bucket = "${var.environment}-${var.short_region}-${var.bucket_name}"
+    force_destroy = var.bucket_force_destroy
+    control_object_ownership = var.bucket_control_object_ownership
+    object_ownership         = var.bucket_object_ownership
+}
---
+++ terraform/test.tfvars
@@ -0,0 +1,7 @@
+environment = "test"
+region = "us-west-2"
+short_region = "usw2"
+bucket_name = "example-197t371t"
+bucket_force_destroy = true
+bucket_control_object_ownership = true
+bucket_object_ownership = true
